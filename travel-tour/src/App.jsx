734edb3417d134b612eab8fc76f3a6c55d17f580
htetsoemoe <soemoehtet.mdy@gmail.com>
2023-06-05 17:44:57 +0630

    finished video overlay text layers in home.jsx

--- travel-tour/src/App.jsx
+++ travel-tour/src/App.jsx
@@ -8,8 +8,8 @@ const App = () => {
   return (
     <>
       <Navbar />
-      {/* <Home />
-      <Main />
+      <Home />
+      {/* <Main />
       <Footer /> */}
     </>
   )
--- travel-tour/src/Components/Home/Home.jsx
+++ travel-tour/src/Components/Home/Home.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import './home.css'
+import video from '../../assets/video1.mp4'
 
 const Home = () => {
   return (
-    <div>
-      <h1>This is home.</h1>
-    </div>
+    <section className="home">
+      <div className="overlay"></div>
+      <video src={video} muted autoPlay loop type="video/mp4"></video>
+
+      <div className="homeContent container">
+        <div className="textDiv">
+          <span className="smallText">
+            Our Packages
+          </span>
+          <h1 className="homeTitle">
+            Search your holidays
+          </h1>
+        </div>
+      </div>
+    </section>
   )
 }
 
