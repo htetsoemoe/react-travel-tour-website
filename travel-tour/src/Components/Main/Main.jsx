408e823085be9232f8589372484d3aad3aaa844b
htetsoemoe <soemoehtet.mdy@gmail.com>
2023-06-09 18:52:19 +0630

    finished card layout in main.jsx

--- travel-tour/src/Components/Main/Main.jsx
+++ travel-tour/src/Components/Main/Main.jsx
@@ -1,11 +1,58 @@
 import React from 'react'
 import './main.css'
+import { HiLocationMarker } from 'react-icons/hi'
+import { HiOutlineClipboardCheck } from 'react-icons/hi'
+
+// import JSON from assets/destination.js
+import destinations from '../../assets/destination'
 
 const Main = () => {
   return (
-    <div className='main-heading'>
-      <h1>This is Main.</h1>
-    </div>
+    <section className='main container section'>
+      <div className="secTitle">
+        <h3 className="title">
+          Most Visited Destinations
+        </h3>
+      </div>
+
+      <div className="secContent grid">
+        {
+          destinations.map((destination) => {
+            return (
+              <div key={destination.id} className="singleDestination">
+                <div className="imageDiv">
+                  <img src={destination.image} alt={destination.destTitle} />
+                </div>
+
+                <div className="cardInfo">
+                  <h1 className="destTitle">{destination.destTitle}</h1>
+                  <span className="continent flex">
+                    <HiLocationMarker className="icon" />
+                    <span className="name">{destination.location}</span>
+                  </span>
+
+                  <div className="fees flex">
+                    <div className="grade">
+                      <span>{destination.grade} <small>+1</small> </span>
+                    </div>
+                    <div className="price">
+                      <h4>{destination.fees}</h4>
+                    </div>
+                  </div>
+
+                  <div className="desc">
+                    <p>{destination.description}</p>
+                  </div>
+                  <button className="btn flex">
+                    DETAILS <HiOutlineClipboardCheck className="icon" />
+                  </button>
+                </div>
+              </div>
+            )
+          })
+        }
+      </div>
+    </section>
   )
 }
 
---
+++ travel-tour/src/assets/destination.js
@@ -0,0 +1,24 @@
+const data = [
+    {
+        id: 1,
+        image: '../src/assets/img/bora.jpg',
+        destTitle: 'Bora Bora',
+        location: 'New Zealand',
+        grade: 'CULTURAL RELAX',
+        fees: '$700',
+        description: 'The island of Bora Bora is a honeymooner’s paradise. The ultimate setting for romance is largely mainly due to the iconic beauty of Mount Otemanu,which rises from the sea like an emerald jewel, encircled by gorgeous coral reefs and several white sand islets called motus.'
+    },
+    {
+        id: 2,
+        image: '../src/assets/img/Ijen_Crater.jpg',
+        destTitle: 'Ijen Crater',
+        location: 'Indonesia',
+        grade: 'GOE RELAX',
+        fees: '$800',
+        description: 'It’s known as the Blue crater, due to the “blue fire” it produces. It’s a hike upwards, and the sulphur that leaks out is toxic so you actually have to wear a mask. The views looks amazing and I’ve heard nothing but fantastic things about the location.'
+    },
+];
+
+export default data;
+
+
