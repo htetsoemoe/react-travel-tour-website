28f411932ecea48ffd7f32534c1595c0880b3742
htetsoemoe <soemoehtet.mdy@gmail.com>
2023-06-12 12:03:29 +0630

    finished card design

--- travel-tour/src/assets/destination.js
+++ travel-tour/src/assets/destination.js
@@ -10,12 +10,75 @@ const data = [
     },
     {
         id: 2,
-        image: '../src/assets/img/Ijen_Crater.jpg',
-        destTitle: 'Ijen Crater',
+        image: '../src/assets/img/santorini.jpg',
+        destTitle: 'Santorini',
+        location: 'Greece',
+        grade: 'CULTURAL RELAX',
+        fees: '$2500',
+        description: 'Santorini, known since ancient times as Thira, is one of the most famous islands in the world. The fact that you can sit in front of the caldera, enjoy local dishes, a drink or a coffee while gazing at the remarkable beauty of an active volcano is priceless!'
+    },
+    {
+        id: 3,
+        image: '../src/assets/img/paris.jpg',
+        destTitle: 'Eiffel Tower',
+        location: 'France',
+        grade: 'HONEYMOON',
+        fees: '$3000',
+        description: 'Eiffel tower has become known as a symbol of romance. There are many reasons why the Eiffel Tower has become a romantic destination. For one, the tower\'s design is elegant and timeless, with its intricate ironwork and graceful curves creating a romantic atmosphere.'
+    },
+    {
+        id: 4,
+        image: '../src/assets/img/gbf.jpg',
+        destTitle: 'Great Barrier Reef',
+        location: 'Australia',
+        grade: 'ADVENTURE RELAX',
+        fees: '$3500',
+        description: 'The Great Barrier Reef is the world\'s largest coral reef system, composed of over 2, 900 individual reefs and 900 islands stretching for over 2, 300 kilometres over an area of approximately 344, 400 square kilometres.'
+    },
+    {
+        id: 5,
+        image: '../src/assets/img/float-hotel.jpg',
+        destTitle: 'River Kwai Jungle Rafts ',
+        location: 'Thailand',
+        grade: 'HONEYMOON RELAX',
+        fees: '$1000',
+        description: 'Imagine a place where a river flows under your bed, and the stars provide most of the lighting at night.  This is the unplugged experience at the River Kwai Jungle Rafts in Kanchanaburi Province in Thailand.  It’s a floating bamboo lodge moored on the River Kwai Noi, surrounded by lush green mountains and jungle.'
+    },
+    {
+        id: 6,
+        image: '../src/assets/img/golden-bridge.jpg',
+        destTitle: 'The Golden Bridge',
+        location: 'Vietnam',
+        grade: 'HONEYMOON RELAX',
+        fees: '$1500',
+        description: 'The Golden Bridge in Vietnam is at the top of the travel bucket list for many tourists. Since its opening day – it has topped the global architectural charts. Over the past few years, unique bridges have sprung all over the world.'
+    },
+    {
+        id: 7,
+        image: '../src/assets/img/java.jpg',
+        destTitle: 'Java Islands',
         location: 'Indonesia',
-        grade: 'GOE RELAX',
-        fees: '$800',
-        description: 'It’s known as the Blue crater, due to the “blue fire” it produces. It’s a hike upwards, and the sulphur that leaks out is toxic so you actually have to wear a mask. The views looks amazing and I’ve heard nothing but fantastic things about the location.'
+        grade: 'CULTURE ADVENTURE RELAX',
+        fees: '$1300',
+        description: 'Java, lying between Sumatra and Bali, is a volcano-dotted island that’s at the geographic and economic center of Indonesia, and home to more than half its people. Java’s largest city is modern, sprawling Jakarta, the nation’s capital. It’s home to the expansive National Museum, an old town (Kota Tua) with Dutch colonial buildings, as well as upscale hotels and shopping malls.'
+    },
+    {
+        id: 8,
+        image: '../src/assets/img/singapore.jpg',
+        destTitle: 'Singapore',
+        location: 'Singapore',
+        grade: 'CULTURE RELAX',
+        fees: '$1500',
+        description: 'A global financial center, being among the most densely populated places in the world, having a world-class city airport with a waterfall, and a Botanic Garden that is a World Heritage Site.'
+    },
+    {
+        id: 9,
+        image: '../src/assets/img/taj-mahal.jpg',
+        destTitle: 'Taj Mahal',
+        location: 'India',
+        grade: 'CULTURE RELAX',
+        fees: '$1000',
+        description: 'The Taj Mahal is an ivory-white marble mausoleum on the right bank of the river Yamuna in Agra, Uttar Pradesh, India. It was commissioned in 1631 by the fifth Mughal emperor, Shah Jahan to house the tomb of his favourite wife, Mumtaz Mahal; it also houses the tomb of Shah Jahan himself.'
     },
 ];
 
